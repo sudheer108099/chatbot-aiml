d96eba9519e03c85b58819bbe36fdf4d089873b0
kushagra <kushgpt08@gmail.com>
2021-03-17 17:00:49 +0530

    eva

--- src/main.py
+++ src/main.py
@@ -1,108 +1,123 @@
 import aiml
 import os.path as path
-import sqlite3 as sq
+import pickle
+# import sqlite3 as sq
 
-SCHEMA = [
-    """create table if not exists User (
-        id INTEGER PRIMARY KEY NOT NULL,
-        name TEXT,
-        address TEXT,
-        phone TEXT
-    );""",
-    """CREATE TABLE IF NOT EXISTS Contacts (
-        patient INT ,
-        name TEXT,
-        phone TEXT UNIQUE,
-        relation TEXT,
-        FOREIGN KEY(patient) REFERENCES User(id)
-    );""",
-    """create table if not exists Converstation (
-        user_req TEXT,
-        bot_reply TEXT
-    );""",
-]
+# SCHEMA = [
+#     """create table if not exists User (
+#         id INTEGER PRIMARY KEY NOT NULL,
+#         name TEXT,
+#         address TEXT,
+#         phone TEXT
+#     );""",
+#     """CREATE TABLE IF NOT EXISTS Contacts (
+#         patient INT ,
+#         name TEXT,
+#         phone TEXT UNIQUE,
+#         relation TEXT,
+#         FOREIGN KEY(patient) REFERENCES User(id)
+#     );""",
+#     """create table if not exists Converstation (
+#         user_req TEXT,
+#         bot_reply TEXT
+#     );""",
+# ]
 
-DB_FILE = "db.sqlite3"
-DATABASE = sq.connect(DB_FILE)
+# DB_FILE = "db.sqlite3"
+# DATABASE = sq.connect(DB_FILE)
 
+# def create_account():
+#     """Create a new user account. Asks for a bunch of info and puts it in relevant tables."""
+#     name = ''
+#     while not name:
+#         name = input("What's your name? ")
+#     address = ''
+#     while not address:
+#         address = input("What's your address? ")
+#     contact = input("What's your phone? ")
+#     family = []
+#     while True:
+#         if input("Do you want to add family contact? ") == 'y':
+#             family_name = input("Name of your family member? ")
+#             relation = input("Who is this person to you? ")
+#             fam_contact = input("What is the person's phone number? ")
+#             family.append((family_name, relation, fam_contact))
+#         else:
+#             break
+#     sql = "INSERT INTO User (name, address, phone) VALUES (?, ?, ?);"
+#     cursor = DATABASE.cursor()
+#     cursor.execute(sql, (name, address, contact))
+#     id_ = cursor.lastrowid
+#     sql = "INSERT INTO Contacts (patient, name, relation, phone) VALUES (?, ?, ?, ?);"
+#     cursor.executemany(sql, [(id_, *i) for i in family])
+#     DATABASE.commit()
 
-def create_account():
-    """Create a new user account. Asks for a bunch of info and puts it in relevant tables."""
-    name = ''
-    while not name:
-        name = input("What's your name? ")
-    address = ''
-    while not address:
-        address = input("What's your address? ")
-    contact = input("What's your phone? ")
-    family = []
-    while True:
-        if input("Do you want to add family contact? ") == 'y':
-            family_name = input("Name of your family member? ")
-            relation = input("Who is this person to you? ")
-            fam_contact = input("What is the person's phone number? ")
-            family.append((family_name, relation, fam_contact))
-        else:
-            break
-    sql = "INSERT INTO User (name, address, phone) VALUES (?, ?, ?);"
-    cursor = DATABASE.cursor()
-    cursor.execute(sql, (name, address, contact))
-    id_ = cursor.lastrowid
-    sql = "INSERT INTO Contacts (patient, name, relation, phone) VALUES (?, ?, ?, ?);"
-    cursor.executemany(sql, [(id_, *i) for i in family])
-    DATABASE.commit()
+# def init():
+#     cursor = DATABASE.cursor()
+#     for schema in SCHEMA:
+#         cursor = cursor.execute(schema)
+#     cursor.execute("INSERT OR REPLACE INTO User VALUES (0, NULL, NULL, NULL)")
+#     cursor.execute(
+#         "INSERT OR REPLACE INTO Contacts VALUES (0, 'Police', '100', 'Policia')"
+#     )
+#     cursor.execute(
+#         "INSERT OR REPLACE INTO Contacts VALUES (0, 'Ambulance', '102', 'Save ME!')"
+#     )
+#     cursor.execute(
+#         "INSERT OR REPLACE INTO Contacts VALUES (0, 'Fire Brigade', '101', 'Fire Fire!')"
+#     )
+#     DATABASE.commit()
 
-
-def init():
-    cursor = DATABASE.cursor()
-    for schema in SCHEMA:
-        cursor = cursor.execute(schema)
-    cursor.execute("INSERT OR REPLACE INTO User VALUES (0, NULL, NULL, NULL)")
-    cursor.execute(
-        "INSERT OR REPLACE INTO Contacts VALUES (0, 'Police', '100', 'Policia')"
-    )
-    cursor.execute(
-        "INSERT OR REPLACE INTO Contacts VALUES (0, 'Ambulance', '102', 'Save ME!')"
-    )
-    cursor.execute(
-        "INSERT OR REPLACE INTO Contacts VALUES (0, 'Fire Brigade', '101', 'Fire Fire!')"
-    )
-    DATABASE.commit()
-
+KERNEL_PICKLE = 'botz/kernel'
+LEARNFILE = 'botz/startup.xml'
+INIT_COMMAND = 'LOGIN'
 
 if __name__ == '__main__':
-    init()
-    kernel = aiml.Kernel()
-    kernel.setTextEncoding(None)
+    # init()
+    # kernel = aiml.Kernel()
+    # kernel.setTextEncoding(None)
 
-    if path.exists(brain := path.join('botz', 'brain')):
-        kernel.bootstrap(brainFile=brain)
+    # if path.exists(brain := path.join('botz', 'brain')):
+    #     kernel.bootstrap(brainFile=brain)
+    # else:
+    #     kernel.bootstrap(learnFiles='startup.xml',
+    #                      commands='LOAD AIML B',
+    #                      chdir=f"{aiml.__path__[0]}/botdata/standard")
+    # inp = 'hello'
+    # for _ in range(7):
+    #     bot_res = kernel.respond(inp)
+    #     data = kernel.getSessionData(kernel._globalSessionID)
+    #     print('DATA::', data)
+    #     print('<<<', bot_res)
+    #     inp = input('>>> ')
+    # kernel.saveBrain('botz/brain')
+    # # kernel.bootstrap(learnFiles="startup.xml",
+    # #                  commands="LOAD AIML B",
+    # #                  chdir=f"{aiml.__path__[0]}/botdata/standard")
+
+    # # ans = input(
+    # #     "Would you like to login or create a new account?\nType 'create' to create and any other key to exit\n"
+    # # )
+    # # if ans == 'create':
+    # #     create_account()
+
+    # # while True:
+    # #     user = input(">>> ")
+    # #     if user == 'q' or user == 'quit':
+    # #         break
+    # #     bot = kernel.respond(user)
+    # #     print('>>>', bot)
+    # DATABASE.close()
+    if path.exists(KERNEL_PICKLE):
+        with open(KERNEL_PICKLE, 'rb') as f:
+            kernel = pickle.load(f)
     else:
-        kernel.bootstrap(learnFiles='startup.xml',
-                         commands='LOAD AIML B',
-                         chdir=f"{aiml.__path__[0]}/botdata/standard")
-    inp = 'hello'
-    for _ in range(7):
-        bot_res = kernel.respond(inp)
-        data = kernel.getSessionData(kernel._globalSessionID)
-        print('DATA::', data)
-        print('<<<', bot_res)
-        inp = input('>>> ')
-    kernel.saveBrain('botz/brain')
-    # kernel.bootstrap(learnFiles="startup.xml",
-    #                  commands="LOAD AIML B",
-    #                  chdir=f"{aiml.__path__[0]}/botdata/standard")
-
-    # ans = input(
-    #     "Would you like to login or create a new account?\nType 'create' to create and any other key to exit\n"
-    # )
-    # if ans == 'create':
-    #     create_account()
-
-    # while True:
-    #     user = input(">>> ")
-    #     if user == 'q' or user == 'quit':
-    #         break
-    #     bot = kernel.respond(user)
-    #     print('>>>', bot)
-    DATABASE.close()
+        kernel = aiml.Kernel()
+        kernel.setTextEncoding(None)
+        kernel.bootstrap(learnFiles=LEARNFILE, commands=INIT_COMMAND)
+    while True:
+        user_inp = input('>>> ')
+        if user_inp == 'bye' or user_inp == 'quit':
+            break
+        bot_resp = kernel.respond(user_inp)
+        print('>>>', bot_resp)
