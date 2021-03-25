75afea3b98e8662e0a67f9871ee2dbedb8cae14f
kushagra <kushgpt08@gmail.com>
2021-03-25 17:05:35 +0530

    almost done :)
    Co-authored-by: tejasvrajput7 <tejasvrajput7@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -5,22 +5,24 @@ import webbrowser
 from urllib.parse import quote
 
 BOTZ = 'botz'
+SESSION_ID = 69420
 
 KERNEL_PICKLE = 'kernel'
 LEARNFILE = 'startup.xml'
 INIT_COMMAND = ['LOAD AIML B', "EMERGENCY NUMBERS GALORE"]
 
 if __name__ == '__main__':
+    kernel = aiml.Kernel()
+    kernel.setTextEncoding(None)
+    kernel.bootstrap(learnFiles=LEARNFILE, commands=INIT_COMMAND, chdir=BOTZ)
     if path.exists(pick := path.join(BOTZ, KERNEL_PICKLE)):
         with open(pick, 'rb') as f:
-            kernel = pickle.load(f)
+            session_data = pickle.load(f)
+            for key, val in session_data.items():
+                kernel.setPredicate(key, val, SESSION_ID)
+            bot_resp = kernel.respond('hello', SESSION_ID)
     else:
-        kernel = aiml.Kernel()
-        kernel.setTextEncoding(None)
-        kernel.bootstrap(learnFiles=LEARNFILE,
-                         commands=INIT_COMMAND,
-                         chdir=BOTZ)
-    bot_resp = kernel.respond('login')
+        bot_resp = kernel.respond('login', SESSION_ID)
     while True:
         if bot_resp.startswith('OPEN BROWSER: '):
             bot_resp = bot_resp[14:]
@@ -33,7 +35,7 @@ if __name__ == '__main__':
         user_inp = input('>>> ')
         if user_inp == 'bye' or user_inp == 'quit':
             break
-        bot_resp = kernel.respond(user_inp)
-    print(kernel.getSessionData(kernel._globalSessionID))
-    # with open(pick, 'wb+') as f:
-    #     pickle.dump(kernel, f)
+        bot_resp = kernel.respond(user_inp, SESSION_ID)
+    session_data = kernel.getSessionData(SESSION_ID)
+    with open(pick, 'wb+') as f:
+        pickle.dump(session_data, f)
